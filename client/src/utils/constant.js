2d870514be5a9aa279eef32d1d308afae1b6e7f0
SALHASAID <salhasaid8989@gmail.com>
2022-10-22 19:45:27 +0300

    Initial Commit

---
+++ client/src/utils/constant.js
@@ -0,0 +1,5 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi;
+
+export const contractAddress = '0x45445d8fD0b403B65b2f02176420B9ed31083Fa3';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,13 @@
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.9',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/yqd5w5wz5eyq_CnMnIniI0aKGHNsMy_n',
+      accounts: [ '0858354bee72f3dd874c8f09953d5144c59d52e4cb725d91a34e77b49c8c8c0c']
+    }
+  }
+}
+              
